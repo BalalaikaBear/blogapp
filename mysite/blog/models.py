07aa61bb94abae007bec6fee7a78cc9c81c1476c
BalalaikaBear <eightes@yandex.ru>
2025-01-08 17:46:23 +0300

    None

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,37 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    # создание класса перечисления на основе Enum
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'  # Черновик
+        PUBLISHED = 'PB', 'Published'  # Опубликован
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(  # связь "многие к одному"
+        settings.AUTH_USER_MODEL,  # AUTH_USER_MODEL по умолчанию указывает на auth.User
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT
+    )
+
+    class Meta:
+        # Сортировка, применяемая по умолчанию.
+        # Индекс, повышает производительность запросов. Создается отдельный индекс в таблице.
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self):
+        return self.title
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig'  # Класс BlogConfig - это конфигурация приложения
 ]
 
 MIDDLEWARE = [
